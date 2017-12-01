195543b3c0de90a00c0bfdf2c0b691cf38706854
lydsnyder <lrsbp7@mail.missouri.edu>
2017-12-01 14:20:17 -0600

    dre is awesome

--- Christmas/newtest/app.js
+++ Christmas/newtest/app.js
@@ -1,5 +1,7 @@
 angular.module('christmasLists', [])
 	.controller('listController', function( $scope ) {
+        $scope.activeMember;
+        $scope.activeMemberData;
         $scope.members = [ 
             {
                 "name": "rebekah",
@@ -9,15 +11,28 @@ angular.module('christmasLists', [])
                 "name": "sarah",
                 "file": "sarah.json"
             }
-        ]
+        ];
+        $scope.selectMember = function( member ){
+            $scope.activeMember = member;
+        };
+        $scope.loadMemberData = function( member )
+        {
+            $scope.selectMember( member );  
+            $scope.requestResource( $scope.activeMember.file, function( results )
+                {
+                $scope.activeMemberData = results;
+                console.log( $scope.activeMemberData );
+                }
+            );
+        };
     /* we have a scope variable called request resource
         that we assign a function to                    */
-        $scope.requestResource = function( filename ){ 
+        $scope.requestResource = function( filename, onComplete ){ 
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                    // Typical action to be performed when the document is ready:
-                   document.getElementById("demo").innerHTML = xhttp.responseText;
+                   onComplete( this.responseText );
                 }
             };
             xhttp.open("GET", filename, true);
